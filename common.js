a79c299a058f170183cc20e8da579841836ea7c0
nono050517 <cinry1128@yahoo.co.jp>
2026-05-08 04:56:54 +0900

    Update common.js

--- common.js
+++ common.js
@@ -54,6 +54,7 @@ async function loadSidebar() {
                 if (item.page_id === 'seedlancer') link = '2job_seedlancer.html';
                 if (item.page_id === 'fencer') link = '2job_fencer.html';
                 if (item.page_id === 'battlesmith') link = '2job_battlesmith.html';
+                if (item.page_id === 'bishop') link = '2job_bishop.html';
                 html += `<li><a href="${link}">${item.display_name}</a></li>`;
             });
             html += `</ul>`;
