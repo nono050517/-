30bfee65670f939d6636ee13209151b2401ba34b
nono050517 <cinry1128@yahoo.co.jp>
2026-05-07 22:57:08 +0900

    Update common.js

--- common.js
+++ common.js
@@ -48,6 +48,7 @@ async function loadSidebar() {
                 if (item.page_id === 'grenadier') link = '2job_grenadier.html';
                 if (item.page_id === 'meister') link = '2job_meister.html';
                 if (item.page_id === 'alchemist') link = '2job_alchemist.html';
+                if (item.page_id === 'musketeer') link = '2job_musketeer.html';
                 html += `<li><a href="${link}">${item.display_name}</a></li>`;
             });
             html += `</ul>`;
